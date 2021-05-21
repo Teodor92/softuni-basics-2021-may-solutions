a2f5eb2b70e4d1b476ae0b9202c5dab568b3f1ab
teodor.kurtev <teodor.ivanov92@gmail.com>
2021-05-21 16:54:39 +0300

    feat: added template project

---
+++ solutions/03.ConditionalStatementsAdvanced/07.WorkingHours/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _07.WorkingHours
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
