714a7bcf242e186f59cb3ad6aff6f7f9a8abe0fa
Teodor Kurtev <teodor.ivanov92@gmail.com>
2021-05-28 15:16:17 +0300

    feat: added skeleton for the new lecture

---
+++ solutions/04.ForLoop/03.Numbers1ToNWithStep3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _03.Numbers1ToNWithStep3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ solutions/04.ForLoop/11.CleverLily/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _11.CleverLily
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
