70ca2eafc7bdd8c093ca434317b047ddd1bb9a94
Teodor Kurtev <teodor.ivanov92@gmail.com>
2021-06-04 13:34:44 +0300

    feat: added templates for the new leacture

---
+++ solutions/05.WhileLoop/04.Sequence2kPlus1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _04.Sequence2kPlus1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ solutions/05.WhileLoop/06.MaxNumber/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _06.MaxNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
